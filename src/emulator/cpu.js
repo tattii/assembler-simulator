0f833c1541f963061feea6b7f832ebd9badb9931
tattii <yuta.tatti@gmail.com>
2015-05-16 02:57:31 +0900

    complete SIMPLE assembler

--- src/emulator/cpu.js
+++ src/emulator/cpu.js
@@ -63,6 +63,7 @@ app.service('cpu', ['memory', function(memory) {
             self.carry = false;
             self.fault = false;
             self.v = false;
+			self.output = 0;
         }
     };
 
